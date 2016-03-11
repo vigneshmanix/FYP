946459c976c082a4509706354a8c607974310aed
Vignesh M <vigneshmanix@gmail.com>
2016-03-11 15:12:56 +0530

    add comments and data source

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 #Final Year Project
 
+ - Download extract data from https://archive.ics.uci.edu/ml/machine-learning-databases/00340/data.zip
  - Install python packages `pip install -r requirements.txt`
  - Run extractFeature to extract feature and store as csv
  - Run buildModelAndPredict to build the model and plot the graphs
--- calcOnePat.m
+++ calcOnePat.m
@@ -1,6 +1,12 @@
 function [ outdata ] = calcOnePat( onepat )
 %CALCONEPAT calculates heartrate and average blood pressure of one patient
-%   Detailed explanation goes here
+%   onepat is one cell with three signal channels
+%   onepatECG is the ECG signal
+%   findpeaks finds local peaks and MinPeakProminence is set to half of max
+%   locs has the location of the peak, pks has the peak value
+%   The difference of locs will give distance between the peaks
+%   We take mean over all the waves
+%   The second channel has the blood pressure. avgbp is mean of blood pressure
 onepatECG = onepat(3,:);
 [pks, locs] = findpeaks(onepatECG,'MinPeakProminence',max(onepatECG)/2);
 heartrate = mean(diff(locs));
